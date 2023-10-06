1c97e7c06e4a6063bb9db6fd49e34c7ad0a4302d
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-10-06 09:39:47 -0300

    commit bia

---
+++ teste1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $text = "O perigo da verdade não é que computadores passem a pensar como humanos, mas sim que humanos passem a pensar como computadores";
+
+    $length = strpos ($text, "computadores passem");
+
+    $result = substr($text, 0, 20); 
+
+        echo $result;
+    ?>
+</body>
+</html>
